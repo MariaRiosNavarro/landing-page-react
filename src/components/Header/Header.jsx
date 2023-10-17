505d4566c650422b1cf7aa06cb6b24ca064d47d0
SuperCoder <propscode254@gmail.com>
2023-10-17 17:20:28 +0200

    add mixin & variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,29 @@
 const Header = () => {
   return (
     <>
-      <h3>header</h3>
+      <header>
+        <h3>Hello There</h3>
+        <h1>We Are Glint</h1>
+        <hr />
+        <ul>
+          <li>
+            <h2>127</h2>
+            <span>Award Received</span>
+          </li>
+          <li>
+            <h2>1505</h2>
+            <span>Cups of Coffe</span>
+          </li>
+          <li>
+            <h2>109</h2>
+            <span>Projects Completed</span>
+          </li>
+          <li>
+            <h2>102</h2>
+            <span>Happy Clients</span>
+          </li>
+        </ul>
+      </header>
     </>
   );
 };
