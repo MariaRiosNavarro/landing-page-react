7bfd5e510d849e8d4bb5c621928a63d802a372ac
SuperCoder <propscode254@gmail.com>
2023-10-17 15:54:15 +0200

    add card and cardcontainer

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import "./Card.scss";
+
+const Card = (src, pfad, title, price) => {
+  return (
+    <>
+      <div className="flex-colum">
+        <img src={src} alt={pfad} />
+        <h3 className="card-title">{title}</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+          praesentium similique error laboriosam rerum, dolorem vel nam
+          dignissimos voluptatibus et.
+        </p>
+        <h4>Aenean</h4>
+        <h3 className="card-price">{price}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,9 +1,21 @@
 import "./CardsContainer.scss";
+import meals from "../../assets/db/mealspicture";
+import Card from "../Card/card";
 
 const CardsContainer = () => {
   return (
     <>
-      <h3>CardContainer</h3>
+      <div className="grid">
+        {meals.map((meal, index) => {
+          <Card
+            key={index}
+            src={meal.src}
+            alt={meal.pfad}
+            title={meal.title}
+            price={meal.price}
+          ></Card>;
+        })}
+      </div>
     </>
   );
 };
