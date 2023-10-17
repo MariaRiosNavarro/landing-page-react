426135e97c1bd1561389a698335e601ac7cadef6
SuperCoder <propscode254@gmail.com>
2023-10-17 18:12:12 +0200

    main finish

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,34 @@
 import "./Main.scss";
 
+const features = [
+  { title: "Brand Identify", logo: "/icons/brush.svg" },
+  { title: "Ilustration", logo: "/icons/object-select.svg" },
+  { title: "Marketing", logo: "/icons/megaphone.svg" },
+  { title: "Web Design", logo: "/icons/globe-line.svg" },
+];
+
 const Main = () => {
   return (
     <>
-      <h3>Main</h3>
+      <div className="main">
+        <h3>What We Do</h3>
+        <h2>We´ve got Everything you need to launch and grow your business</h2>
+        <section className="grid-section">
+          {features.map((feature, index) => (
+            <article key={index}>
+              <img src={feature.logo} alt={feature.title} />
+              <h4>{feature.title}</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+                fugit possimus! Aliquid eveniet quos quis quidem at doloremque
+                fugit recusandae laudantium. Placeat, esse facere impedit
+                aliquam veniam labore earum soluta reprehenderit asperiores,
+                nemo rem harum eaque.
+              </p>
+            </article>
+          ))}
+        </section>
+      </div>
     </>
   );
 };
